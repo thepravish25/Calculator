7eebdc491a425f6ee8f0a2d7d808a71dfef36fb0
Pravish Pandey <115358440+thepravish25@users.noreply.github.com>
2022-12-19 14:52:48 +0530

    Add files via upload

---
+++ calculatorproject.py
@@ -0,0 +1,36 @@
+#take input from user
+a= int(input("Enter the first number:"))
+#take input from user
+b= int(input("Enter the second number:"))
+sum= int(a)+int(b) #sum
+subtraction= int(a)-int(b) #subtraction
+multiplication= int(a)*int(b) #multiplication
+division= int(a)/int(b) #division
+modulus= int(a)%int(b) #modulus
+floordivision= int(a)//int(b) #floordivision
+exponential= int(a)**int(b) #exponential
+#printing sum
+print("the sum is:",sum)
+#printing subtraction
+print("the subtaction is:",subtraction)
+#printing multiplication
+print("the multiplication is:",multiplication)
+#printing division
+print("the division is:",division)
+#printing modulus
+print("the modulus is:",modulus)
+#printing floordivision
+print("the  floordivision is:",floordivision)
+#printing exponential
+print("the exponential is:",exponential)
+
+#output:
+# Enter the first number:5
+# Enter the second number:5
+# the sum is: 10
+# the subtaction is: 0
+# the multiplication is: 25
+# the division is: 1.0
+# the modulus is: 0
+# the  floordivision is: 1
+# the exponential is: 3125
